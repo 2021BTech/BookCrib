3a85a77f44f2171fbd154e11acbd67b1d73a17e6
2021BTech <daffydanni@gmail.com>
2023-06-26 10:41:15 -0400

    Working fine

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+//API
+import { useGlobalContext } from "../../context.";
+//componenets
+import Book from "../BookList/Book";
+import Loading from "../Loader/Loader";
+import coverImg from "../../Images/error-3.jpg";
 //styles
 import "./BookList.css";
 
+//https://covers.openlibrary.org/b/id/240727-S.jpg
+
 const BookList = () => {
-  return <div></div>;
+  const { books, loading, resultTitle } = useGlobalContext();
+  const booksWithCovers = books.map((singleBook) => {
+    return {
+      ...singleBook,
+      // removing /works/ to get only id
+      id: singleBook.id.replace("/works/", ""),
+      cover_img: singleBook.cover_id
+        ? `https://covers.openlibrary.org/b/id/${singleBook.cover_id}-L.jpg`
+        : coverImg,
+    };
+  });
+
+  if (loading) return <Loading />;
+
+  return (
+    <section className="booklist">
+      <div className="container">
+        <div className="section-title">
+          <h2>{resultTitle}</h2>
+        </div>
+        <div className="booklist-content grid">
+          {booksWithCovers.slice(0, 30).map((item, index) => {
+            return <Book key={index} {...item} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BookList;
--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 //styles
 import "./Loader.css";
+//loader
+import LoaderImg from "../../Images/loader.svg";
 
 const Loader = () => {
-  return <div></div>;
+  return (
+    <div className="loader flex flex-c">
+      <img src={LoaderImg} alt="loader" />
+    </div>
+  );
 };
 
 export default Loader;
